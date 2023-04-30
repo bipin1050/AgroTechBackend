6c538b1ecf369f2657d4da61769f8a9b18bd617a
Akash <075bct010.akash@pcampus.edu.np>
2023-04-30 16:25:48 +0545

    OTP

--- utility/nodemailer.js
+++ utility/nodemailer.js
@@ -15,15 +15,16 @@ module.exports.sendMail = async function sendMail(str, data) {
   if (str == "signup") {
     Osubject = `Thank you for signing ${data.name}`;
     var id=data._id;
-    var link="http://localhost:3000/verification/"+id
     Ohtml = `
     <h1>Welcome to Agro-Tech</h1>
     <p>Hope you have a good time ahead!</p>
     <p>Your details:</p>
     <p>Name: ${data.name}</p>
     <p>Email: ${data.email}</p>
-    <a href={link}>Click here</a>
+    <a id="mailid" href="#">Click here</a>
     `;
+    var link=document.getElementById("mailid")
+    link.href="http://localhost:3000/verification/"+id
   } else if (str == "resetpassword") {
     Osubject = `Reset Password`;
     Ohtml = `
